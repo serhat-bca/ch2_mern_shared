52c47db4ef7ab9643004b5c93618f9b7f8a9c70e
serhat-bca <sersen@bergen.org>
2025-10-23 12:14:02 -0400

    input field added.

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import Movie from "./components/Movie";
 
 const App = ({ movies }) => {
+  const [movielist, setMovielist] = useState(movies);
+  const [movieName, setMovieName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Label: ", movieName);
+    setMovieName("");
+  };
+
+  const handleChange = (e) => {
+    setMovieName(e.target.value);
+  };
+
   return (
     <div>
       <h2>Movies</h2>
       <ul>
-        {movies.map((m) => (
+        {movielist.map((m) => (
           <Movie key={m.id} movie={m} />
         ))}
       </ul>
+      <h3>Add a new movie</h3>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} value={movieName} />
+        <button type="submit">Add Movie</button>
+      </form>
     </div>
   );
 };
